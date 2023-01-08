52f889650eab7d65d8135db6b6c0a5f2265b58c0
wootak96 <90369173+wootak96@users.noreply.github.com>
2023-01-08 14:44:54 +0900

    Create 1806.py

---
+++ wootak96/백준 5주차/1806.py
@@ -0,0 +1,26 @@
+n,s=map(int,input().split())
+a = list(map(int,input().split()))
+
+left=right=0
+sum=a[0]
+ans=n+1
+while left<=right and right<n:
+    if sum<s:
+        right += 1
+        if right < n:
+            sum += a[right]
+    elif sum == s:
+        ans = min(right-left+1,ans)
+        right += 1
+        if right < n:
+            sum += a[right]
+    elif sum > s:
+        ans = min(right-left+1,ans)
+        sum -= a[left]
+        left += 1
+        if left > right and left < n:
+            right=left
+            sum = a[left]
+if ans > n:
+    ans=0
+print(ans)
